108d913d43cd1c2703f7d25b332cbde947fdcd67
fonnymunkey <fonnymunkey@gmail.com>
2026-03-10 04:22:59 -0400

    1.0.1 bump version

--- src/main/java/fonnymunkey/gibbed/Gibbed.java
+++ src/main/java/fonnymunkey/gibbed/Gibbed.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.relauncher.Side;
 )
 public class Gibbed {
     public static final String MODID = "gibbed";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "Gibbed";
 
     @Mod.Instance(MODID)
